8b57f7748fd7a02720a6572d2aee233ebe15770f
marsru <zrw1215225@163.com>
2019-06-20 15:15:04 +0800

    zrw

--- src/lianxi/lianxi.java
+++ src/lianxi/lianxi.java
@@ -8,13 +8,24 @@ public class lianxi {
 	public static void main(String[] args) {
 		
 		
+
 		int a = 0;
 		if(a>0)
 		       System.out.println(111);
 		else
 		       System.out.println(22);
 		
+		int a1 = 0;
+		if(a>0)
+		       System.out.println(111);
+		else
+		       System.out.println(22);
 		
+		int a2 = 0;
+		if(a>0)
+		       System.out.println(111);
+		else
+		       System.out.println(22);
 		// TODO Auto-generated method stub
         System.out.println(222);
 		System.out.println(111);
