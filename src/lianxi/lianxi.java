af7de471940253d5947db4ff7133081dc254bba4
慕随心 <1154430034@qq.com>
2019-06-20 14:49:17 +0800

    提交

--- src/lianxi/lianxi.java
+++ src/lianxi/lianxi.java
@@ -7,12 +7,13 @@ public class lianxi {
 	       System.out.println(111);
 	else
 	       System.out.println(22);
-
+	
 
 	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
         System.out.println(222);
-        System.out.println("�����Ŵ�˧��");
+        System.out.println("嘻嘻");
 		System.out.println(111);
 		System.out.println("musuixin");
 		System.out.println("8989898");
