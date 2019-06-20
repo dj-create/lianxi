e706203214ace3326cadfcb539789ed736880e9d
chenxuhu <chenxuhu@163.com>
2019-06-20 15:01:03 +0800

    chenxuhu

--- src/lianxi/lianxi.java
+++ src/lianxi/lianxi.java
@@ -2,20 +2,30 @@ package lianxi;
 
 public class lianxi {
 
-	int a = 0;
-	if(a>0)
-	       System.out.println(111);
-	else
-	       System.out.println(22);
+
 	
 
 	public static void main(String[] args) {
-
+		
+		
+		int a = 0;
+		if(a>0)
+		       System.out.println(111);
+		else
+		       System.out.println(22);
+		
+		
 		// TODO Auto-generated method stub
         System.out.println(222);
 		System.out.println(111);
 		System.out.println("musuixin");
 		System.out.println("8989898");
+		
+		
+		if(a>0)
+		       System.out.println(111);
+		else
+		       System.out.println(22);
 
 	}
 
