29411ee17e2aac004860d53995c2efc11102e950
dj-create <52020558+dj-create@users.noreply.github.com>
2019-06-20 11:28:12 +0800

    Update lianxi.java

--- src/lianxi/lianxi.java
+++ src/lianxi/lianxi.java
@@ -2,9 +2,13 @@ package lianxi;
 
 public class lianxi {
 
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println(222);
+System.out.println("我是张大帅哥");
 		System.out.println(111);
 		System.out.println("8989898");
 	}
