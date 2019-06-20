c79168751b7cf1edfb768092b2451d4bf371fc19
慕随心 <1154430034@qq.com>
2019-06-20 11:22:33 +0800

    方党豫

--- src/lianxi/lianxi.java
+++ src/lianxi/lianxi.java
@@ -4,7 +4,7 @@ public class lianxi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(222);
+        System.out.println(222);
 		System.out.println(111);
 	}
 
